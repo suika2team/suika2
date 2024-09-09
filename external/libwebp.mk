7419ce9264c8f459da8f49ef3c4d8cbdbbab5a69
Maiku Kamishiro <info@opennovel.org>
2024-09-09 15:05:29 +0900

    Fix Android x86_64

--- external/libwebp.mk
+++ external/libwebp.mk
@@ -1,4 +1,4 @@
-all: alpha_dec.o buffer_dec.o frame_dec.o idec_dec.o io_dec.o quant_dec.o tree_dec.o vp8_dec.o vp8l_dec.o webp_dec.o anim_decode.o demux.o alpha_processing.o cost.o cost_mips32.o cpu.o dec.o dec_clip_tables.o enc.o filters.o lossless.o lossless_enc.o rescaler.o ssim.o upsampling.o yuv.o alpha_enc.o analysis_enc.o backward_references_cost_enc.o backward_references_enc.o config_enc.o cost_enc.o filter_enc.o frame_enc.o histogram_enc.o iterator_enc.o near_lossless_enc.o picture_csp_enc.o picture_enc.o picture_psnr_enc.o picture_rescale_enc.o picture_tools_enc.o predictor_enc.o quant_enc.o syntax_enc.o token_enc.o tree_enc.o vp8l_enc.o webp_enc.o anim_encode.o muxedit.o muxinternal.o muxread.o bit_reader_utils.o bit_writer_utils.o color_cache_utils.o filters_utils.o huffman_encode_utils.o huffman_utils.o quant_levels_dec_utils.o quant_levels_utils.o random_utils.o rescaler_utils.o thread_utils.o utils.o
+all: alpha_dec.o buffer_dec.o frame_dec.o idec_dec.o io_dec.o quant_dec.o tree_dec.o vp8_dec.o vp8l_dec.o webp_dec.o anim_decode.o demux.o alpha_processing.o cost.o cost_neon.o cost_sse2.o cpu.o dec.o dec_neon.o dec_sse2.o dec_sse41.o dec_clip_tables.o enc.o filters.o filters_neon.o filters_sse2.o lossless.o lossless_neon.o lossless_sse2.o lossless_sse41.o lossless_enc.o lossless_enc_neon.o lossless_enc_sse2.o lossless_enc_sse41.o rescaler.o rescaler_neon.o rescaler_sse2.o ssim.o ssim_sse2.o upsampling.o upsampling_neon.o upsampling_sse2.o upsampling_sse41.o yuv.o yuv_neon.o yuv_sse2.o yuv_sse41.o alpha_enc.o analysis_enc.o backward_references_cost_enc.o backward_references_enc.o config_enc.o cost_enc.o filter_enc.o frame_enc.o histogram_enc.o iterator_enc.o near_lossless_enc.o picture_csp_enc.o picture_enc.o picture_psnr_enc.o picture_rescale_enc.o picture_tools_enc.o predictor_enc.o quant_enc.o syntax_enc.o token_enc.o tree_enc.o vp8l_enc.o webp_enc.o anim_encode.o muxedit.o muxinternal.o muxread.o bit_reader_utils.o bit_writer_utils.o color_cache_utils.o filters_utils.o huffman_encode_utils.o huffman_utils.o quant_levels_dec_utils.o quant_levels_utils.o random_utils.o rescaler_utils.o thread_utils.o utils.o
 	$(AR) rcs $(PREFIX)/lib/libwebp.a *.o
 
 alpha_dec.o: src/dec/alpha_dec.c
@@ -43,13 +43,25 @@ alpha_processing.o: src/dsp/alpha_processing.c
 cost.o: src/dsp/cost.c
 	$(CC) -I. -I./src $(CFLAGS) -c $<
 
-cost_mips32.o: src/dsp/cost_mips32.c
+cost_neon.o: src/dsp/cost_neon.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+cost_sse2.o: src/dsp/cost_sse2.c
 	$(CC) -I. -I./src $(CFLAGS) -c $<
 
 cpu.o: src/dsp/cpu.c
 	$(CC) -I. -I./src $(CFLAGS) -c $<
 
 dec.o: src/dsp/dec.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+dec_neon.o: src/dsp/dec_neon.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+dec_sse2.o: src/dsp/dec_sse2.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+dec_sse41.o: src/dsp/dec_sse41.c
 	$(CC) -I. -I./src $(CFLAGS) -c $<
 
 dec_clip_tables.o: src/dsp/dec_clip_tables.c
@@ -61,22 +73,73 @@ enc.o: src/dsp/enc.c
 filters.o: src/dsp/filters.c
 	$(CC) -I. -I./src $(CFLAGS) -c $<
 
+filters_neon.o: src/dsp/filters_neon.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+filters_sse2.o: src/dsp/filters_sse2.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
 lossless.o: src/dsp/lossless.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+lossless_neon.o: src/dsp/lossless_neon.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+lossless_sse2.o: src/dsp/lossless_sse2.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+lossless_sse41.o: src/dsp/lossless_sse41.c
 	$(CC) -I. -I./src $(CFLAGS) -c $<
 
 lossless_enc.o: src/dsp/lossless_enc.c
 	$(CC) -I. -I./src $(CFLAGS) -c $<
 
+lossless_enc_neon.o: src/dsp/lossless_enc_neon.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+lossless_enc_sse2.o: src/dsp/lossless_enc_sse2.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+lossless_enc_sse41.o: src/dsp/lossless_enc_sse41.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
 rescaler.o: src/dsp/rescaler.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+rescaler_neon.o: src/dsp/rescaler_neon.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+rescaler_sse2.o: src/dsp/rescaler_sse2.c
 	$(CC) -I. -I./src $(CFLAGS) -c $<
 
 ssim.o: src/dsp/ssim.c
 	$(CC) -I. -I./src $(CFLAGS) -c $<
 
+ssim_sse2.o: src/dsp/ssim_sse2.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
 upsampling.o: src/dsp/upsampling.c
 	$(CC) -I. -I./src $(CFLAGS) -c $<
 
+upsampling_neon.o: src/dsp/upsampling_neon.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+upsampling_sse2.o: src/dsp/upsampling_sse2.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+upsampling_sse41.o: src/dsp/upsampling_sse41.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
 yuv.o: src/dsp/yuv.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+yuv_neon.o: src/dsp/yuv_neon.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+yuv_sse2.o: src/dsp/yuv_sse2.c
+	$(CC) -I. -I./src $(CFLAGS) -c $<
+
+yuv_sse41.o: src/dsp/yuv_sse41.c
 	$(CC) -I. -I./src $(CFLAGS) -c $<
 
 alpha_enc.o: src/enc/alpha_enc.c
